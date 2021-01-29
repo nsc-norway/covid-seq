4b36f21e7c00feb77f291945344be102377dfa40
arvindsundaram <arvindymsundaram@gmail.com>
2021-01-29 22:07:19 +0100

    Update and rename nsc_pipeline_script.sh to script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+nextflow run viralrecon/main.nf \
+-with-singularity nfcore_viralrecon_1.1.0.sif \
+-profile singularity \
+--input <samplelist.csv> \
+--protocol amplicon \
+--amplicon_bed util/<PREP>_primers.bed \
+--fasta util/NC_045512.2.fasta \
+--skip_kraken2 \
+--skip_assembly 
+
+nextflow run pangolin/main.nf \
+-with-singularity pangolin_nsc_20210129.sif \
+--viralrecon_folder $PWD \
+--samplelist <samplelist.csv>
