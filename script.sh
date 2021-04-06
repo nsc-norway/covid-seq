8082139aa19477eac435c3689a6620f3d9b532d1
Marius Bjørnstad <p.m.bjornstad@medisin.uio.no>
2021-04-06 11:18:45 +0200

    Updated script for the current version

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
-nextflow run /boston/runScratch/analysis/pipelines/2021_covid19/nsc_pipeline_v7/main.nf \
+nextflow run main.nf \
     --outpath "$PWD" \
     --samplelist sampleList.csv \
     --align_tool "bowtie2" \
     -resume > pipeline_log.txt
-
-nextflow run /boston/runScratch/analysis/pipelines/2021_covid19/report_generator_v7/main.nf
-    
