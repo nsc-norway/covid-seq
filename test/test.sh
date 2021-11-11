4a3051dd388a978f372af61669b52d6b85cabc61
Marius Bjørnstad <pmb@fa2k.net>
2021-11-11 11:13:45 +0100

    Update test and reference

--- test/test.sh
+++ test/test.sh
@@ -8,7 +8,7 @@ mkdir run || echo "Directory 'run/' already exists, will resume execution."
 if nextflow run ../main.nf --test --lab test --samplelist sampleList_test.csv --outpath run -resume -w run/work "$@"
 then
     echo " -- Comparing report file with refrence --"
-    if diff run/results/report_v9.tsv fasit/report_v9.tsv
+    if diff run/results/report_v10.tsv fasit/report_v10.tsv
     then
         echo " -----"
         echo "TEST PASSED"
